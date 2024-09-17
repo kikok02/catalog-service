e9bffe76d72c466a6c89fcb19fb0ad33e77b54d2
Felipe Ferreira Albuquerque <74032267+kikok02@users.noreply.github.com>
2024-09-17 17:17:53 -0300

    Delete src directory

--- src/main/java/com/polarbookshop/catalog_service/domain/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.polarbookshop.catalog_service.domain;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String isbn) {
-        super("The book with ISBN " + isbn + " was not found.");
-    }
-}
